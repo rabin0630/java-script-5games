247ec8938e6b163c15a5a60230506e520e89ba16
SottoJunRabin <gunaid1231@outlook.jp>
2026-05-09 06:15:28 +0900

    feat: スライドパズル画像のランダム選択とホバー表示機能の実装
    - ランダムな画像選択処理の追加と画像パスの修正
    - 「完成図を見る」ボタンの mouseover / mouseleave による画像表示切り替えを追加

--- ts/modules/slide.ts
+++ ts/modules/slide.ts
@@ -1,12 +1,16 @@
 const menuPageEl = document.querySelector(".sp-cover") as HTMLElement;
 const backToMenuEl = document.querySelector(".sp-back-to-menu") as HTMLElement;
 const gamePageEl = document.querySelector(".sp-container") as HTMLElement;
-
+const originalImageEl = document.querySelector(
+  "#sp-original-image",
+) as HTMLElement;
+const showOriginalBtnEl = document.querySelector("#sp-show-original-btn");
 const levelOptions = document.querySelectorAll(
   ".sp-menu > li",
 ) as NodeListOf<HTMLLIElement>;
-
 let selectedLevel;
+let selectedImage;
+const images = ["space", "veges"];
 
 const showGamePage = (): void => {
   menuPageEl.classList.add("hide");
@@ -22,10 +26,27 @@ levelOptions.forEach((option) => {
   option.addEventListener("click", () => {
     showGamePage();
     selectedLevel = option.dataset.level;
+    setOriginalImage();
   });
 });
 
 backToMenuEl.addEventListener("click", () => {
   showMenuPage();
   selectedLevel = "";
+});
+
+const setOriginalImage = () => {
+  selectedImage = images[Math.floor(Math.random() * images.length)];
+  originalImageEl?.setAttribute(
+    "src",
+    `./src/images/slide_puzzle/${selectedImage}/${selectedImage}.png`,
+  );
+};
+
+showOriginalBtnEl?.addEventListener("mouseover", () => {
+  originalImageEl.classList.add("show");
+});
+
+showOriginalBtnEl?.addEventListener("mouseleave", () => {
+  originalImageEl.classList.remove("show");
 });
